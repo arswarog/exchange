df67d933dc299074923b66cb0d5e839745363717
Evgeny <arswarog@yandex.ru>
2022-12-18 15:53:44 +0300

    add loading state to amount input

--- src/components/AmountInput.tsx
+++ src/components/AmountInput.tsx
@@ -1,13 +1,17 @@
+import classNames from 'classnames';
+
 interface IProps {
     amount: number;
+    loading?: boolean;
     onChange?(value: number): void;
 }
 
-export function AmountInput({ amount, onChange }: IProps): JSX.Element {
+export function AmountInput({ amount, loading, onChange }: IProps): JSX.Element {
     const changeHandler = (e: any) => onChange?.(+e.target.value);
 
     return (
         <input
+            className={classNames({ loading: loading })}
             onChange={changeHandler}
             value={amount}
             type="number"
